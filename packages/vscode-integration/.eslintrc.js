b8ccffef260536b848f87ee684c15a1134ff4707
Tobias Ortmayr <tortmayr@eclipsesource.com>
2021-05-25 12:28:46 +0200

    #60 Clean up yarn and CI build
    - Update metadata of all packages
    - Move glue code packages into dedicated `packages` directory
    - Align version numbers to 0.9.0
    -  Introduce global 'build' and 'lint' script
    - Enable eslint reporting & publishing in Jenkinsfile
    
    Part of eclipse-glsp/glsp/issues/60

--- packages/vscode-integration/.eslintrc.js
+++ packages/vscode-integration/.eslintrc.js
@@ -1,9 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
     extends: [
-        '../configs/base.eslintrc.json',
-        '../configs/warnings.eslintrc.json',
-        '../configs/errors.eslintrc.json'
+        '../../configs/base.eslintrc.json',
+        '../../configs/warnings.eslintrc.json',
+        '../../configs/errors.eslintrc.json'
     ],
     parserOptions: {
         tsconfigRootDir: __dirname,
