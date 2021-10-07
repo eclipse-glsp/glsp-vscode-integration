6f6ae7f8cef7e7551e9825324bde29ba174e3e11
Tobias Ortmayr <tortmayr@eclipsesource.com>
2021-10-07 16:12:14 +0200

    #408 Update prettier config and fix reflect-metadata error (#19)
    * #408 Update prettier config and fix reflect-metadata error
    
    Update to latest @eclipse-glsp/config version. This version ships with prettier as dependency which enables to configure a workspace-local prettier version for the vscode prettier extension. This ensures that all developers are using the same prettier version.
    
    .settings:
    - Remove prettier  as general default formatter to avoid unintended usage of prettier for languages like java.
    - Configure prettier as default formatter for CSS
    - Set the prettier path to "node_modules/prettier"
    
    Workflow-example:
    - Fix issue with reflect-metadata polyfill: Extract import of "reflect-metadata" in dedicated "index.ts" file to ensure that the polyfill is loaded before any dependent sprotty or glsp modules are imported.
    
    Reformat all code:
    Reformat code with  "yarn prettier --config .prettierrc ./ --write" in root to ensure that all files for which prettier is set as default formatter are properly formatted.
    
    -Remove obsolete .editorconfig file
    
    * Update yarn.lock
    
    Co-authored-by: Philip Langer <planger@eclipsesource.com>

--- example/workflow/webview/src/app.ts
+++ example/workflow/webview/src/app.ts
@@ -17,13 +17,14 @@ import { createWorkflowDiagramContainer } from '@eclipse-glsp-examples/workflow-
 import { GLSPStarter } from '@eclipse-glsp/vscode-integration-webview';
 import '@eclipse-glsp/vscode-integration-webview/css/glsp-vscode.css';
 import { Container } from 'inversify';
-import 'reflect-metadata';
 import { SprottyDiagramIdentifier } from 'sprotty-vscode-webview';
 
-export class WorkflowGLSPStarter extends GLSPStarter {
+class WorkflowGLSPStarter extends GLSPStarter {
     createContainer(diagramIdentifier: SprottyDiagramIdentifier): Container {
         return createWorkflowDiagramContainer(diagramIdentifier.clientId);
     }
 }
 
-new WorkflowGLSPStarter();
+export function launch(): void {
+    new WorkflowGLSPStarter();
+}
