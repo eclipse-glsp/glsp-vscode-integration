f8202b835799ee8905b526ff7b9a28ea849ed6f1
Tobias Ortmayr <tortmayr@eclipsesource.com>
2021-12-10 02:09:10 +0100

    Switch glsp dependencies back to next

--- example/workflow/scripts/download.ts
+++ example/workflow/scripts/download.ts
@@ -18,20 +18,20 @@ import download from 'mvn-artifact-download';
 import { join } from 'path';
 
 const downloadDir = join(__dirname, '../extension/server');
-const mavenRepository = 'https://oss.sonatype.org/content/groups/public/';
+const mavenRepository = 'https://oss.sonatype.org/content/repositories/snapshots/';
 const groupId = 'org.eclipse.glsp.example';
 const artifactId = 'org.eclipse.glsp.example.workflow';
-const version = '0.9.0';
+const version = '0.10.0';
 const classifier = 'glsp';
 
-if (!existsSync(`${__dirname}/../extension/server/${artifactId}-${version}-${classifier}.jar`)) {
+if (!existsSync(`${__dirname}/../extension/server/${artifactId}-${version}-${classifier}-SNAPSHOT.jar`)) {
     console.log('Downloading latest version of the Workflow Example Java Server from the maven repository...');
-    download({ groupId, artifactId, version, classifier, isSnapShot: false }, downloadDir, mavenRepository)
+    download({ groupId, artifactId, version, classifier, isSnapShot: true }, downloadDir, mavenRepository)
         .then(() =>
             console.log(
                 'Download completed. Start the server using this command: \njava -jar org.eclipse.glsp.example.workflow-' +
                     version +
-                    '-glsp.jar org.eclipse.glsp.example.workflow.launch.ExampleServerLauncher\n\n'
+                    '-SNAPSHOT-glsp.jar org.eclipse.glsp.example.workflow.launch.ExampleServerLauncher\n\n'
             )
         )
         .catch(err => console.error(err));
