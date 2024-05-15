d6654e456ae3fa7e4bbed3b53da3c8fdda2fe5ce
Tobias Ortmayr <tortmayr@eclipsesource.com>
2024-05-15 16:43:53 +0200

    GLSP-1320: Update to latest glsp dev config
    - Updates to latest version of @eclipse-glsp/dev
    - Add generate:index utility script
    - Regenerate index files
    - Fix codeActionsOnSave in vscode settings
    - Refactor upgrade:next script
    - Reformat code base with prettier
    - Fix copyright headers (by running a full check with glsp checkHeaders . -t full)
    Part of eclipse-glsp/glsp#1320

--- packages/vscode-integration-webview/src/glsp-starter.ts
+++ packages/vscode-integration-webview/src/glsp-starter.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2018 TypeFox and others.
+ * Copyright (c) 2018-2024 TypeFox and others.
  * Modifications: (c) 2020-2023 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
--- packages/vscode-integration/src/browser/re-export.ts
+++ packages/vscode-integration/src/browser/re-export.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2020-2024 EclipseSource and others.
+ * Copyright (c) 2024 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -13,17 +13,4 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-
-export * from './default-modules';
-export * from './diagram-identifier';
-export * from './features/copyPaste/copy-paste-module';
-export * from './features/copyPaste/copy-paste-startup';
-export * from './features/default/default-module';
-export * from './features/default/extension-action-handler';
-export * from './features/default/vscode-glsp-modelsource';
-export * from './features/export/export-module';
-export * from './features/navigation/navigation-module';
-export * from './features/notification/notification-module';
-export * from './glsp-diagram-widget';
-export * from './glsp-starter';
-export * from './webview-glsp-client';
+export * from '../common';
